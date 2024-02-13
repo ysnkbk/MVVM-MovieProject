d122fef9c52cc36f41b8ff24a3eb7e2f67e820e5
Yasin Kabak <yasinkabak257@gmail.com>
2024-02-13 15:35:41 +0300

    Update APIHeaders.swift

--- MVVM-MovieProject/Utilities/APIHeaders.swift
+++ MVVM-MovieProject/Utilities/APIHeaders.swift
@@ -11,7 +11,7 @@ enum APIHeaders {
     static func headers() -> [String: String] {
         return [
             "accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMGMyZGE0NzNmNmFiMGE1MTVmMWVjYjY4ZmNkMWJkMCIsInN1YiI6IjY1OGViNjIyY2EwZTE3NjA3YmJhNzJiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0vWjxzsMI4cyUxmkIcw8mECrfOqbk1tBiTvGAD30M7U"
+            "Authorization": "API KEY."
         ]
     }
 }
